9cf41d62c96e91eedc019e5925d538fac4459b6b
namoah23@my.whitworth.edu <namoah23@my.whitworth.edu>
2019-12-05 09:36:28 -0800

    coding good no errors atm

--- source/final.cpp
+++ source/final.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 using namespace std;
-
 //Function Prototypes
 char shelfname_stringtoChar_Row(string shelfIndex);
 char shelfname_stringtoChar_Column(string shelfIndex);
 int numerical_Value_ofRowindex(char shelfname_stringtoChar_Row);
 int numerical_Value_ofColumnindex(char shelfname_stringtoChar_Column);
-
 
 class  Customer{
 public:
@@ -14,13 +12,15 @@ public:
     double receipt_bill; // stores the total purchase of the customer
     int cash_rewards; //a promotional something, I have not figured it out so do not ask me what it does. 
     int customer_ratings;
+    int key;//key used to perform a specific fxn to the shelf.
 
     //Constructor for this class. When the class is istantiated default values are given to the data fields during compile time. 
     Customer(){
         authorization = false;
         receipt_bill = 0.00;
         cash_rewards = 0.00;
-        customer_ratings = 0;    
+        customer_ratings = 0; 
+        key = 1235;//default value for the data. 
     }
 
     //Methods for this class
@@ -62,11 +62,13 @@ public:
     bool authorisation;//authorises the functions of this class to be used. 
     string shelf_select;//check your previous library on how to make small letters capital , check lm8 it might be there.
     bool assistance;//
+    int key;//grants access to perform a specific access to the shelf.
 
     Stocker(){
         authorisation = false;
         shelf_select = "aA";//Default shell value
         assistance = false;
+        key = 1234;//default value for the key.
     }
 
     bool authorising(){//authorises this class to be used in session.
@@ -116,7 +118,15 @@ public:
     /*For the Profits Method, I have to be really critical of this. I will do this after I have found how the code is going to work. */
 };
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//This lists of functions, help correct the Users input of a shelf name, by converting if included lower case letters to Uppercase Letters . 
+//This lists of functions, consists of the shelfs of the store and function manipulators that helps access the shelf array based on the users input.
+//This function helps the stocker to add quanity amount to the shelf.
+int quantity_add(){
+  int add_quant = 0;
+  cout << "Enter the quanity amount you want to add--> ";
+  cin >> add_quant;
+  
+  return add_quant;
+}
 char chartoNum(char ch){
   if( static_cast<int>(ch)>=48&&static_cast<int>(ch)<58){//returns the numbers if user input are numbers
     return ch;//returns the ch when the input is a number.
@@ -162,6 +172,7 @@ int specific_ASCIIofChar_shelfIndex(int totalASCII_of_index){
   }
   //return 8;
 }
+
 string warehouseItems(string warehouseIndex){  
   const int item_type = 5;
   //correctshelfName(), evaluates the user input to the standard
@@ -173,30 +184,71 @@ string warehouseItems(string warehouseIndex){
     warehouse_items[2] = "Smartphones";
     warehouse_items[3] = "Books";
     warehouse_items[4] = "Fast food";
-
-
-
   /* The numerical values of the row and column index are gotten and then assiged to int varibles*/
   int numerical_val_row = numerical_Value_ofRowindex(shelfname_stringtoChar_Row(warehouseIndex)); //The numerical value of Row index is gotten is evaluated by calling the functions. 
   int numerical_val_column = numerical_Value_ofColumnindex(shelfname_stringtoChar_Column(warehouseIndex));//The numerical value of column index is evaluated by calling the functions.
-
   /* After I have gotten the numerical values, I send it to a function that uses switch statements to evaluate them and couts a unique
   value that can be used by the warehouse to give a specific index of the shelf
   */
   int total_numerical_valRow_Clmn = numerical_val_row + numerical_val_column;//this variables stores the total numerical value of row index and column index. 
-  
+
   //cout << total_numerical_valRow_Clmn << endl; used for testing purposes
   int index_to_be_accessed = specific_ASCIIofChar_shelfIndex(total_numerical_valRow_Clmn);
-
   //cout << index_to_be_accessed << endl;used for testing purposes. 
   string item_to_disp;
-  
-  for(int i = 0; i < item_type; i++){
+  for(int i = 0; i <= index_to_be_accessed; i++){
     item_to_disp = warehouse_items[i];
   }
-  
   return item_to_disp;
 }
+int warehouseStock(string warehouseIndex){//This contains the quantities of stock in the warehouse with the prize.
+  const int item_type = 5;
+  //correctshelfName(), evaluates the user input to the standard
+  warehouseIndex = correctshelfName(warehouseIndex);
+ /*For compile reasons, this array is hard coded with predefined stuffs and time convenience */
+   int warehouse_items[item_type];
+    warehouse_items[0] = 4;
+    warehouse_items[1] = 3;
+    warehouse_items[2] = 2;
+    warehouse_items[3] = 2;
+    warehouse_items[4] = 3;
+  /* The numerical values of the row and column index are gotten and then assiged to int varibles*/
+  int numerical_val_row = numerical_Value_ofRowindex(shelfname_stringtoChar_Row(warehouseIndex)); //The numerical value of Row index is gotten is evaluated by calling the functions. 
+  int numerical_val_column = numerical_Value_ofColumnindex(shelfname_stringtoChar_Column(warehouseIndex));//The numerical value of column index is evaluated by calling the functions.
+  /* After I have gotten the numerical values, I send it to a function that uses switch statements to evaluate them and couts a unique
+  value that can be used by the warehouse to give a specific index of the shelf*/
+  int total_numerical_valRow_Clmn = numerical_val_row + numerical_val_column;//this variables stores the total numerical value of row index and column index. 
+  //cout << total_numerical_valRow_Clmn << endl; used for testing purposes
+  int index_to_be_accessed = specific_ASCIIofChar_shelfIndex(total_numerical_valRow_Clmn);
+  /*                                                                           */
+  
+  //cout << index_to_be_accessed << endl;used for testing purposes. 
+  int quantity_to_disp;//this variables displays the quantity of the specific shelf to the user.
+  //The loops displays the specific warehouse shelf Item quanity asked for.
+  for(int i = 0; i <=index_to_be_accessed; i++){
+    quantity_to_disp = warehouse_items[i];
+  }
+  cout << "Quanity is " << quantity_to_disp << endl;
+  /* This block of code will ask the Stocker is she wants to add extra quantity of the item to the specific shelf 
+   If the user opts not to add more quantities to the shelf the shel wont increase but if she opts to then it will increase*/
+  int yes_or_no = 0;
+  cout << "Do you want to add more quanties to the shelf Index--> (1)->Yes || (0)-->No >> " << endl;
+  cin >> yes_or_no;
+  if(yes_or_no == 1){
+    warehouse_items[index_to_be_accessed] += quantity_add();
+    //Then the new quanity is displayed rather than the old run. 
+    for(int i = 0; i <= index_to_be_accessed; i++){
+    quantity_to_disp = warehouse_items[i];
+    }
+
+    cout << "New Quanity is--> " << quantity_to_disp << endl;
+  }
+  else{
+    ;//when nothing happens. 
+  } 
+  return quantity_to_disp;//either it returns the old quanity or the accumulated new quantity quanity. 
+}
+//This functions help convert the users input into int values to help access the shelf arrays.
 int numerical_Value_ofRowindex(char convert_rowchar_toInt){
   /*This function converts the rowIndex char gotten from shelfname_stringtoChar_Row to its corresponding ASCII value (integer value)
   */
@@ -218,27 +270,23 @@ char shelfname_stringtoChar_Row(string shelfIndex){//The parameter is the shelf 
     for(int i = 0; i <1; i++){
      string_index_to_char_row = shelfIndex[i];
     }
-    cout << string_index_to_char_row << endl;
+    //For testing reasons, I included the cout.
+    //cout << string_index_to_char_row << endl;
     return string_index_to_char_row;
 }
 char shelfname_stringtoChar_Column(string shelfIndex){//The parameter is the shelf place the user wants to access. 
   // This function breaks the userinput(Shelfindex of column to be evaluated by the add new item category)
  
-  
   char string_index_to_char_column; 
   //char column_index;
   for(int i = 0; i <2; i++){
 
     string_index_to_char_column = shelfIndex[i];
   }
-  cout << string_index_to_char_column << endl;
+  //For testing reasons, I included the the cout below. 
+  //cout << string_index_to_char_column << endl;
   return string_index_to_char_column;
 }
-
-
-
-
-
 
 void myStore(){
     Customer User;// Instantiated the customer class
@@ -248,6 +296,8 @@ void myStore(){
 
     Cashier.shelf_select = Cashier.selectShelf();
     
+    cout << warehouseItems(Cashier.shelf_select) << ":"/*<< " :Quanitiy is --> " */<< endl <</*"New Quanity is-->"<< */warehouseStock(Cashier.shelf_select);//" " << warehouseStock(Cashier.shelf_select) << endl;
+
     
 
 
@@ -260,9 +310,13 @@ void myStore(){
 }
 
 int main(){
+    int program = 1;
 
-   
- myStore();
+    while(program != 0){
+
+        myStore();
+
+    }
     
 
 
